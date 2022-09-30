cd3534efc4420ce8c2e51eef0449fc8bcd1fd6d2
Wisnu Saputro <wisnuvb@gmail.com>
2022-10-01 06:02:28 +0700

    add storybook

---
+++ .storybook/theme.js
@@ -0,0 +1,9 @@
+import { create } from "@storybook/theming";
+
+export default create({
+  base: "light",
+  brandTitle: "Oriviu UI",
+  brandUrl: "https://oriviu.com",
+  brandImage: "https://oriviu.com/image/logo-full.png",
+  brandTarget: "_self",
+});
